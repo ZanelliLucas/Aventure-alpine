4d8058ca64c495ba2c2e35ef2ae2924ded28abd8
Lucas Zanelli <lucas@les-z.org>
2025-03-14 14:58:15 +0100

    six commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+// Importation correcte de Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
